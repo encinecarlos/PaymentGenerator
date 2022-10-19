eb375af47fd5f89c84d5854fdc3f95ff6a91e0e5
Carlos Alexandre Zancki Encine <carlos_alexandre88@hotmail.com>
2022-10-19 07:28:02 -0300

    Ad parallel on implementation

--- PainGeneratorFunction/Generator/PainGenerator.cs
+++ PainGeneratorFunction/Generator/PainGenerator.cs
@@ -151,116 +151,6 @@ namespace PainGeneratorFunction.Generator
                 paymentLevelCList.Clear();
             });
 
-            foreach (var payment in request.Payments)
-            {
-                //var paymentLevelCList = new List<CreditTransferTransactionInformation10CH>();
-                //var paymentLevelB = new PaymentInstructionInformation3CH
-                //{
-                //    PmtInfId = $"PMT-{Guid.NewGuid().ToString().Substring(0, 5)}",
-                //    PmtMtd = PaymentMethod3Code.TRF,
-                //    BtchBookgSpecified = true,
-                //    BtchBookg = true,
-                //    ReqdExctnDt = DateTime.Now,
-                //    Dbtr = new PartyIdentification32CH
-                //    {
-                //        Nm = faker.Name.FullName(),
-                //        PstlAdr = new PostalAddress6CH
-                //        {
-                //            Ctry = "CH",
-                //            AdrLine = new string[] {
-                //                faker.Address.StreetName()
-                //            }
-                //        }
-                //    },
-                //    DbtrAcct = new CashAccount16CH_IdTpCcy
-                //    {
-                //        Id = new AccountIdentification4ChoiceCH
-                //        {
-                //            Item = payment.DebtorAccount,
-                //        }
-                //    },
-                //    DbtrAgt = new BranchAndFinancialInstitutionIdentification4CH_BicOrClrId
-                //    {
-                //        FinInstnId = new FinancialInstitutionIdentification7CH_BicOrClrId
-                //        {
-                //            BIC = faker.Finance.Bic()
-                //        }
-                //    },
-                //};
-
-                //for (var j = 0; j < payment.CreditorQuantity; j++)
-                //{
-                //    var transaction = new CreditTransferTransactionInformation10CH
-                //    {
-                //        PmtId = new PaymentIdentification1
-                //        {
-                //            InstrId = $"INSTRID-{j+1}-{faker.Random.AlphaNumeric(2)}",
-                //            EndToEndId = $"ENDTOENDID-{faker.Random.AlphaNumeric(5)}"
-                //        },
-                //        Amt = new AmountType3Choice
-                //        {
-                //            Item = new ActiveOrHistoricCurrencyAndAmount
-                //            {
-                //                Ccy = "CHF",
-                //                Value = paymentAmount
-                //            }
-                //        },
-                //        Cdtr = new PartyIdentification32CH_Name
-                //        {
-                //            Nm = faker.Name.FullName(),
-                //            PstlAdr = new PostalAddress6CH
-                //            {
-                //                StrtNm = faker.Address.StreetName(),
-                //                BldgNb = faker.Address.BuildingNumber(),
-                //                PstCd = faker.Address.ZipCode(),
-                //                TwnNm = faker.Address.CitySuffix(),
-                //                Ctry = "CH"
-                //            }
-                //        },
-                //        CdtrAcct = new CashAccount16CH_Id
-                //        {
-                //            Id = new AccountIdentification4ChoiceCH
-                //            {
-                //                Item = payment.CreditorAccount
-                //            }
-                //        },
-                //        RmtInf = new RemittanceInformation5CH
-                //        {
-                //            Strd = new StructuredRemittanceInformation7
-                //            {
-                //                CdtrRefInf = new CreditorReferenceInformation2
-                //                {
-                //                    Tp = new CreditorReferenceType2
-                //                    {
-                //                        CdOrPrtry = new CreditorReferenceType1Choice
-                //                        {
-                //                            Item = "QRR"
-                //                        }
-                //                    },
-                //                    Ref = "210000000003139471430009017"
-                //                },
-                //                AddtlRmtInf = new string[]
-                //                {
-                //                        $"ref info test {faker.Random.AlphaNumeric(5)}"
-                //                }
-                //            }
-                //        }
-
-                //    };
-
-                //    totalAmount += (transaction.Amt.Item as ActiveOrHistoricCurrencyAndAmount).Value;
-
-                //    paymentLevelCList.Add(transaction);
-                //}
-
-                //counter += paymentLevelCList.Count;
-
-                //paymentLevelB.CdtTrfTxInf = paymentLevelCList.ToArray();
-
-                //paymentList.Add(paymentLevelB);
-                //paymentLevelCList.Clear();
-            }
-
             painDocument.CstmrCdtTrfInitn.GrpHdr.CtrlSum = totalAmount;
             painDocument.CstmrCdtTrfInitn.GrpHdr.NbOfTxs = counter.ToString();
             painDocument.CstmrCdtTrfInitn.PmtInf = paymentList.ToArray();
@@ -268,11 +158,10 @@ namespace PainGeneratorFunction.Generator
             var pain001 = new XmlSerializer(painDocument.GetType());
 
             using var ms = new MemoryStream();
-
+            
             var xmlSettings = new XmlWriterSettings()
             {
                 Encoding = new UTF8Encoding(false),
-                Indent = true,
             };
 
             using var writer = XmlWriter.Create(ms, xmlSettings);
